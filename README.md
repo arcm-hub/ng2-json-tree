78bac90e384e3ebf96b688b6d3037a3245b1ab7d
Ashok Raja CM <achoppam@cisco.com>
2020-09-07 16:44:44 +0530

    Minor changes

--- README.md
+++ README.md
@@ -1,3 +1,153 @@
-# ng2-json-tree
+# `ng2-json-tree` - XML/JSON Tree for Angular2
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
+# You might find this project helpful if
+- You want to render editable tree from XML / JSON. Primarily developed XML in mind.
+- You want to have basic skeleton to start with tree.
+
+# Main Features
+- **Render JSON/XML** Renders JSON directly on UI. Use XML converter to render XML.
+- **Pagination** Supports pagination at each node level to avoid slowness on browserside.
+- **Custom Toolbar Actions/Buttons** Supports custom actions/buttons at each node level to have custom actions.
+- **Customizable Styles** Supports custom CSS class names to brand according to your needs.
+
+# Let's start
+
+## Installation
+
+To include the package into your **Angular** project, simply use the standard npm package installation command:
+
+```
+npm install ng2-json-tree --save
+```
+
+Please note that the package has a _peer dependency_ on **@angular/core**.
+
+## Usage
+
+Once the module `ng2-json-tree` has been added to a project as described above, it provides the following component exports:
+
+* `ng2-json-tree`: Tree rendering component
+
+The below code snippets assume the use of TypeScript.
+
+### Step 1 - Importing this library with an Angular Module
+
+Import the **Angular module** to your **Angular module**.
+
+```ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Ng2JsonTreeModule } from 'ng2-json-tree';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    Ng2JsonTreeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+```
+
+### Step 2 - Rendering simple tree with an Angular Component
+
+
+TypeScript
+```ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example1',
+  templateUrl: './example1.component.html',
+  styleUrls: ['./example1.component.css']
+})
+export class Example1Component implements OnInit {
+  json = {
+    "note": {
+        "body": {
+            "_text": "Don't forget me this weekend!"
+        },
+        "from": {
+            "_text": "Jani"
+        },
+        "heading": {
+            "_text": "Reminder"
+        },
+        "to": {
+            "_text": "Tove"
+        }
+    }
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+```
+
+HTML Template
+```html
+<ng2-json-tree [json]='json'></ng2-json-tree>
+```
+Output:
+
+
+## Pagination
+
+Config:
+```json
+  config = {
+    pageSize: 2
+  };
+```
+
+HTML Template:
+```html
+<ng2-json-tree [json]='json' [config]='config'></ng2-json-tree>
+```
+
+Output:
+
+## Events
+HTML Template:
+```html
+<ng2-json-tree [json]='json' [config]='config' (event)='onEvent($event)'></ng2-json-tree>
+```
+
+## Custom Toolbar Buttons
+
+HTML Template:
+```html
+<ng2-json-tree [json]='json' [config]='config' (event)='onEvent($event)'></ng2-json-tree>
+```
+
+Type Script Event Handling:
+```ts
+  onEvent(event: any) {
+    var backgroundColor = 'white'
+    if(event.type === 'TREE_NODE_CREATED' && event.data.modelKey === 'food') {
+      event.data.toolbar.append('span')
+      .attr('class', 'ng2-json-tree-toolbar-button')
+      .text('highlight')
+      .on('click', function(d, i) {
+        backgroundColor = backgroundColor === 'white' ? 'lightyellow' : 'white';
+        event.data.d3ParentContainer.style('background-color', backgroundColor);
+        console.log("CLICKED", event);
+      });
+    }
+  }
+```
+
+Output:
+
+## Demo Project
+
+For a more complete worked example of how this module can be used, please see: 
+* Github repo: [tomwanzek/d3-ng2-demo](https://github.com/tomwanzek/d3-ng2-demo) and the related [_live_ Github page](https://tomwanzek.github.io/d3-ng2-demo/).
--- projects/ng2-json-tree/src/lib/ng2-json-tree.component.ts
+++ projects/ng2-json-tree/src/lib/ng2-json-tree.component.ts
@@ -16,6 +16,18 @@ export class Ng2JsonTreeComponent {
     // private obj: any;
     @Output() event = new EventEmitter<any>();
 
+    private _config = {};
+
+    get config(): any {
+        return this._config;
+    }
+
+    @Input()
+    set config(val: any) {
+        this._config = val;
+        this.renderTree();
+    }
+
     private data; // private property _item
 
     // use getter setter to define the property
@@ -25,8 +37,6 @@ export class Ng2JsonTreeComponent {
     
     @Input()
     set json(val: any) {
-      console.log('previous item = ', this.data);
-      console.log('currently selected item=', val);
       this.data = val;
       this.renderTree();
     }
@@ -56,31 +66,29 @@ export class Ng2JsonTreeComponent {
             dpElement = d3.select(this.parentNativeElement);
     
             var rootDiv = dpElement.append('div')
-            // rootDiv.append('div').text('root');
-            // var rootDElement = rootDiv.append('div');
             this.renderSection(rootDiv, this.data);
         }
     }
 
     public getPaginatedData(model: any, page: number = -1):any {
-        var skipList = ['_attributes'];
-        var pageSize = 2;
+        var skipList = this.config.skipList ? this.config.skipList : ['_attributes'];
+        var pageSize = this.config.pageSize ? this.config.pageSize : 0;
         var data = Array.isArray(model) ? model : Object.keys(model).filter(key => !skipList.includes(key));
         var data = data.sort();
         var pagination = {
             total: data.length,
             pageSize: pageSize,    
             current_page: page, 
-            last_page: Math.ceil(data.length / pageSize),
+            last_page: pageSize === 0 ? 1 : Math.ceil(data.length / pageSize),
             from: ((page -1) * pageSize) + 1,
-            to: page  * pageSize,
-            enabled: page !== -1            
+            to: pageSize === 0 ? data.length : page * pageSize,
+            enabled: page !== -1 || pageSize === 0          
         };
-        pagination['data'] = page === -1 ? data : data.slice(pagination.from - 1, pagination.to)
+        pagination['data'] = (page === -1 || pageSize === 0) ? data : data.slice(pagination.from - 1, pagination.to)
         return pagination;
     }
 
-    public renderSection(d3element: any, model: any, page: number = -1) {
+    public renderSection(d3element: any, model: any, page: number = -1, level: number = 1) {
         d3element.selectAll("*").remove();
         if(!model)
           return;
@@ -88,7 +96,6 @@ export class Ng2JsonTreeComponent {
         var d3 = this.d3;
         var pagination = this.getPaginatedData(model, page);
         var data = pagination.data;
-        console.log("LDKFJ", page, pagination);
 
         var rootElement = Array.isArray(model) ? d3element.append('ol').attr('start', pagination.from).style('margin', '0px') : d3element;
         var subElement = Array.isArray(model) ? 'li' : 'div';
@@ -118,52 +125,69 @@ export class Ng2JsonTreeComponent {
                     header.append('div')
                         .text(key=>key)
                         .style("display", "inline-block")
-                        .style("width", "150px") //Parameter
+                        // .style("width", "150px") //Parameter
+                        .attr('class', 'ng2-json-tree-input-label')
                         .attr("id", key=>"_" + key + "_header");
-                    header.append('input')
+                    
+                    var textInput = header.append('input')
                         .attr("value", d=>model[d]._text)
-                        .style("width", "150px")
+                        // .style("width", "150px")
+                        .attr('class', 'ng2-json-tree-input')
                         .on('change', function(d, i, e) {
+                            _this.raiseEvent('TEXT_INPUT_CHANGED', {
+                                d3Element: header,
+                                d3Parent: root,
+                                parentModel: model,
+                                textInput: textInput,
+                                model: model[d],
+                                modelKey: d,
+                                level: level,
+                                oldValue: model[d]['_text'],
+                                newValue: e[0].value,
+                                d3
+                              })
                             model[d]['_text'] = e[0].value;
                         })
+
                     _this.raiseEvent('TEXT_INPUT_CREATED', {
                       d3Element: header,
                       d3Parent: root,
                       parentModel: model,
+                      textInput: textInput,
                       model: model[d],
                       modelKey: d,
+                      level: level,
                       d3
                     })
                 } else {
                     let headerTitle = header.append('span')
                                     .text(key=>key)
-                                    .attr('class', 'tree-node');
+                                    .attr('class', 'ng2-json-tree-node');
 
                     let toolbar = header.append('div').style('display', 'inline-block').style('margin-left', '10px');
                     
                     let currentPage = 1;
                     let tButtons = {
                         prev: toolbar.append('div')
-                                .attr('class', 'toolbar-button')
+                                .attr('class', 'ng2-json-tree-toolbar-button')
                                 .style('display', 'none')
                                 .text("<")
                                 .on("click", function(d, i) {
-                                    if(d3.select(this).attr('class') === 'toolbar-button-disabled')
+                                    if(d3.select(this).attr('class') === 'ng2-json-tree-toolbar-button-disabled')
                                         return;
                                     var detail = root.select("#_" + d + "_detail");
                                     _this.renderSection(detail, model[d], --currentPage);
                                     _this.renderPagination(tButtons, _this.getPaginatedData(model[d], currentPage), true);
                                 }),
                         text: toolbar.append('span')
-                                .attr('class', 'toolbar-page-text')
-                                .style('display', 'none')
-                                .text("1 of 10"),
+                                .attr('class', 'ng2-json-tree-toolbar-page-text')
+                                .style('display', 'none'),
                         next: toolbar.append('div')
-                                .attr('class', 'toolbar-button')
+                                .attr('class', 'ng2-json-tree-toolbar-button')
                                 .style('display', 'none')
                                 .text(">")
                                 .on("click", function(d, i) {
-                                    if(d3.select(this).attr('class') === 'toolbar-button-disabled')
+                                    if(d3.select(this).attr('class') === 'ng2-json-tree-toolbar-button-disabled')
                                         return;
                                     var detail = root.select("#_" + d + "_detail");
                                     _this.renderSection(detail, model[d], ++currentPage);
@@ -174,22 +198,20 @@ export class Ng2JsonTreeComponent {
 
                     root.append('div')
                         .attr("id", name=>"_" + name + "_detail")
-                        .style('margin-left', '20px')
-                        .style('margin-bottom', '10px')
-                        .style("border-left", "1px solid grey")
+                        .attr('class', 'ng2-json-tree-child-area')
+                                        
                     headerTitle.on('click', function(d, i) {
                         var detail = root.select("#_" + d + "_detail");
                         var expanded = detail.selectAll("*").size() > 0;
                         var pageConfig = _this.getPaginatedData(model[d], currentPage);
                         if(expanded) {
                             _this.renderPagination(tButtons, pageConfig, false);
-                            d3.select(this).attr('class', 'tree-node');
+                            d3.select(this).attr('class', 'ng2-json-tree-node');
                             detail.selectAll("*").remove();
                         } else {
                             _this.renderPagination(tButtons, pageConfig, true);
-                            _this.renderSection(detail, model[d], currentPage);
-                            d3.select(this).attr('class', 'tree-node-expanded');
-                            console.log("Clicked", d, i, detail);
+                            _this.renderSection(detail, model[d], currentPage, level++);
+                            d3.select(this).attr('class', 'ng2-json-tree-node-expanded');
                         }
                     });
                     _this.raiseEvent('TREE_NODE_CREATED', {
@@ -198,6 +220,7 @@ export class Ng2JsonTreeComponent {
                       parentModel: model,
                       model: model[d],
                       modelKey: d,
+                      level: level,
                       d3
                     })
                 }
@@ -206,7 +229,6 @@ export class Ng2JsonTreeComponent {
     }
 
     private renderPagination(buttons, pagination, visible) {
-        console.log("PAGI", pagination);
         if(!visible || pagination.last_page <= 1) {
             buttons.next.style('display', 'none');
             buttons.text.style('display', 'none');
@@ -216,15 +238,15 @@ export class Ng2JsonTreeComponent {
 
         buttons.text.style('display', 'inline-block')
         buttons.next.style('display', 'inline-block')
-                    .attr('class', 'toolbar-button-disabled');
+                    .attr('class', 'ng2-json-tree-toolbar-button-disabled');
         buttons.prev.style('display', 'inline-block')
-                    .attr('class', 'toolbar-button-disabled');
+                    .attr('class', 'ng2-json-tree-toolbar-button-disabled');
 
         if(pagination.current_page < pagination.last_page)
-            buttons.next.attr('class', 'toolbar-button');
+            buttons.next.attr('class', 'ng2-json-tree-toolbar-button');
 
         if(pagination.current_page > 1)
-            buttons.prev.attr('class', 'toolbar-button');
+            buttons.prev.attr('class', 'ng2-json-tree-toolbar-button');
         
         buttons.text.text(pagination.current_page + ' of ' + pagination.last_page);
     }
